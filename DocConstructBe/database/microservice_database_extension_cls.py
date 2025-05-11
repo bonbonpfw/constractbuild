3a1460f05fb8df606514b999af5f3b93818a114f
Serhii Kanyhin <kanygin97@gmail.com>
2025-05-11 20:51:10 +0300

    Remove unused code

--- DocConstructBe/database/microservice_database_extension_cls.py
+++
@@ -1,7 +0,0 @@
-class DatabaseExtension:
-    def __init__(self, microservice, db_session):
-        self.db_session = db_session
-        microservice.teardown_appcontext(self.shutdown_session)
-
-    def shutdown_session(self, exception=None):
-        self.db_session.remove()
